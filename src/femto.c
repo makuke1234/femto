af47e27eab1141239af1487d8b141c6ef21b07cd
makuke1234 <makuke1234@gmail.com>
2022-02-04 14:33:39 +0200

    Tweak status messages

--- src/femto.c
+++ src/femto.c
@@ -156,7 +156,7 @@ bool femto_loop(femtoData_t * restrict peditor)
 					swprintf_s(
 						tempstr,
 						MAX_STATUS,
-						L"Using %s%s line endings.",
+						L"Using %s%s line endings",
 						(pfile->eolSeq & EOL_CR) ? L"CR" : L"",
 						(pfile->eolSeq & EOL_LF) ? L"LF" : L""
 					);
@@ -176,7 +176,7 @@ bool femto_loop(femtoData_t * restrict peditor)
 					swprintf_s(
 						tempstr,
 						MAX_STATUS,
-						L"File reloaded successfully! %s%s line endings.",
+						L"File reloaded successfully! %s%s line endings",
 						(pfile->eolSeq & EOL_CR) ? L"CR" : L"",
 						(pfile->eolSeq & EOL_LF) ? L"LF" : L""
 					);
@@ -201,7 +201,7 @@ bool femto_loop(femtoData_t * restrict peditor)
 					swprintf_s(tempstr, MAX_STATUS, L"Memory allocation error!");
 					break;
 				default:
-					swprintf_s(tempstr, MAX_STATUS, L"Wrote %d bytes.", saved);
+					swprintf_s(tempstr, MAX_STATUS, L"Wrote %d bytes", saved);
 				}
 			}
 			else if (boolGet(keybuffer, sac_Ctrl_E) && !boolGet(prevkeybuffer, sac_Ctrl_E))
