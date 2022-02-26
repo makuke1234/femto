022804fcc33a1a9c8696dd95d0451a4af4768e7e
makuke1234 <makuke1234@gmail.com>
2022-02-27 00:15:07 +0200

    Enrich test

--- testing/test7.c
+++ testing/test7.c
@@ -8,7 +8,15 @@ int wmain(void)
 {
 	setlib("strredup");
 
-	
+	char * str1 = strredup(NULL, "test1");
+	test(str1 != NULL, "supposed not to be NULL");
+	test(strcmp(str1, "test1") == 0, "supposed to be equal");
+	char * str2 = strredup(str1, "test2");
+	test(str2 != NULL, "supposed no to be NULL");
+	test(str1 == str2, "supposed not to reallocate any memory");
+	test(strcmp(str2, "test2") == 0, "supposed to be euqal");
+
+	free(str2);
 
 	return 0;
 }
