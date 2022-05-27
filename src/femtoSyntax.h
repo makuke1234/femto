4bae779d4c0bac489097001e2cfc9d885e8d32bf
makuke1234 <makuke1234@gmail.com>
2022-05-28 01:14:26 +0300

    Beginning of doc

--- src/femtoSyntax.h
+++ src/femtoSyntax.h
@@ -25,6 +25,7 @@
 
 struct femtoLineNode;
 
+// Enum that defines different supported language syntax codes
 enum femtoSyntax
 {
 	fstxNONE,
@@ -44,13 +45,18 @@ enum femtoSyntax
 	fstxSIZE
 };
 
+// Detects syntax from filename
 enum femtoSyntax fSyntaxDetect(const wchar_t * restrict fileName);
 
+// Returns const pointer to syntax name from syntax identifier
 const char * fSyntaxName(enum femtoSyntax fs);
+// Automatically allocates memory for the syntax parser
 bool fSyntaxParseAutoAlloc(struct femtoLineNode * restrict node);
 
+// Tokeniser function signature for fSyntaxParseCLike
 typedef void (*tokeniserFunc_t)(struct femtoLineNode * restrict node, uint32_t start, uint32_t lasti, WORD kwCol);
 
+// Token parsers
 void checkCToken(struct femtoLineNode * restrict node, uint32_t start, uint32_t lasti, WORD kwCol);
 void checkCPPToken(struct femtoLineNode * restrict node, uint32_t start, uint32_t lasti, WORD kwCol);
 void checkPyToken(struct femtoLineNode * restrict node, uint32_t start, uint32_t lasti, WORD kwCol);
@@ -58,20 +64,80 @@ void checkJSToken(struct femtoLineNode * restrict node, uint32_t start, uint32_t
 void checkRustToken(struct femtoLineNode * restrict node, uint32_t start, uint32_t lasti, WORD kwCol);
 void checkGoToken(struct femtoLineNode * restrict node, uint32_t start, uint32_t lasti, WORD kwCol);
 
+// Syntax parsers
+
+/**
+ * @brief Doesn't parse any syntax, just enjoy the view...
+ * 
+ * @param node Pointer to line node
+ * @param colors Colors vector
+ * @return true Success
+ * @return false Failure
+ */
 bool fSyntaxParseNone(struct femtoLineNode * restrict node, const WORD * restrict colors);
+/**
+ * @brief Doesn't parse any syntax, just enjoy the view...
+ * 
+ * @param node Pointer to line node
+ * @param colors Colors vector
+ * @param func Tokeniser function pointer
+ * @param lang Language identifier
+ * @return true Success
+ * @return false Failure
+ */
 bool fSyntaxParseCLike(
 	struct femtoLineNode * restrict node,
 	const WORD * restrict colors,
 	tokeniserFunc_t func,
 	enum femtoSyntax lang
 );
+/**
+ * @brief Parses markdown.
+ * 
+ * @param node Pointer to line node
+ * @param colors Colors vector
+ * @return true Success
+ * @return false Failure
+ */
 bool fSyntaxParseMd(struct femtoLineNode * restrict node, const WORD * restrict colors);
 
+/**
+ * @brief Parses Python... Huh, who uses that?
+ * 
+ * @param node Pointer to line node
+ * @param colors Colors vector
+ * @return true Success
+ * @return false Failure
+ */
 bool fSyntaxParsePy(struct femtoLineNode * restrict node, const WORD * restrict colors);
 
+/**
+ * @brief Let's parse the ominous JSON syntax. Actually, it's not hard hard to do.
+ * 
+ * @param node Pointer to line node
+ * @param colors Colors vector
+ * @return true Success
+ * @return false Failure
+ */
 bool fSyntaxParseJSON(struct femtoLineNode * restrict node, const WORD * restrict colors);
+/**
+ * @brief Who doesn't need a little CSS here and there?!??
+ * 
+ * @param node Pointer to line node
+ * @param colors Colors vector
+ * @return true Success
+ * @return false Failure
+ */
 bool fSyntaxParseCSS(struct femtoLineNode * restrict node, const WORD * restrict colors);
 
+/**
+ * @brief HTML is basically the same as XML
+ * 
+ * @param node Pointer to line node
+ * @param colors Colors vector
+ * @return true Success
+ * @return false Failure
+ */
 bool fSyntaxParseXML(struct femtoLineNode * restrict node, const WORD * restrict colors);
 
 
