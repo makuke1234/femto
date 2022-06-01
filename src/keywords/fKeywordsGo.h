1bdf079eaec52b9ce535f494a4b5cde403990a7d
makuke1234 <makuke1234@gmail.com>
2022-06-01 11:36:09 +0300

    Move keywords to separate files

---
+++ src/keywords/fKeywordsGo.h
@@ -0,0 +1,55 @@
+#ifndef FKEYWORDS_GO_H
+#define FKEYWORDS_GO_H
+
+#include "../fCommon.h"
+
+static const wchar * s_keyWordsGo[] = {
+	L"break",
+	L"case",
+	L"chan",
+	L"const",
+	L"continue",
+	L"default",
+	L"defere",
+	L"else",
+	L"fallthrough",
+	L"for",
+	L"func",
+	L"go",
+	L"goto",
+	L"if",
+	L"import",
+	L"interface",
+	L"map",
+	L"package",
+	L"range",
+	L"return",
+	L"select",
+	L"struct",
+	L"switch",
+	L"type",
+	L"var",
+
+	// Built-in types
+	L"string",
+	L"bool",
+	L"int8",
+	L"uint8",
+	L"byte",
+	L"int16",
+	L"uint16",
+	L"int32",
+	L"rune",
+	L"uint32",
+	L"int64",
+	L"uint64",
+	L"int",
+	L"uint",
+	L"uintptr",
+	L"float32",
+	L"float64",
+	L"complex64",
+	L"complex128"
+};
+
+#endif
